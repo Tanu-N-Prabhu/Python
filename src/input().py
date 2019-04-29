ba009b6acba38b8525664bbec6563c1038034a2e
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2019-04-28 23:42:04 -0600

    Create input().py

---
+++ src/input().py
@@ -0,0 +1,5 @@
+firstName = input("Enter your first name")
+lastName = input("Enter your last name")
+age = input("Enter your age")
+
+print("Your firstname is: %s, lastname is: %s, and height is: %sl" %(firstName, lastName, age))
