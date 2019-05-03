b8030c04e8ef64df7e2565fb3dab4dd0bffb210a
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2019-05-03 17:42:22 -0600

    Create tuples.py

---
+++ src/tuples.py
@@ -0,0 +1,18 @@
+tuple1 = (1, "Hi", True, 8.999)
+
+print(tuple1)
+
+tupleEmpty = ()
+print(tupleEmpty)
+
+# Accessing the tuple by Index
+
+tuple2 = (1, 2, 3, 4, 5)
+
+print(tuple2[4])
+print(tuple2[1])
+
+# Slicing the tuples
+
+print(tuple2[1:3])
+print(tuple2[:3])
