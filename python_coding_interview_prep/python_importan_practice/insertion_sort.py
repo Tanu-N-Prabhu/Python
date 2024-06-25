a83e6e04449feafa7f3c09111e1f66c591c0cd44
Dhiraj Patra <dhiraj.patra@in.ey.com>
2024-06-25 09:24:12 +0530

    updated

---
+++ python_coding_interview_prep/python_importan_practice/insertion_sort.py
@@ -0,0 +1,31 @@
+"""
+Sorts a list in ascending order using insertion sort algorithm.
+
+  Args:
+      arr: The list to be sorted.
+
+  Returns:
+      None. Sorts the list in-place.
+"""
+def insertion_sort(arr):
+    n = len(arr)
+    if n <= 1:
+        return
+
+    for i in range(1, n):
+        j = arr[i - 1]
+        key = arr[i]
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+            # Insert the key at its correct position
+            arr[j + 1] = key
+
+
+if __name__=="__main__":
+    # Example usage
+    my_list = [8, 4, 2, 10, 1, 5]
+    insertionSort(my_list)
+    print(my_list)  # Output: [1, 2, 4, 5, 8, 10]
